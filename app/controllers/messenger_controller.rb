68cfade0400376960a1d3c1d8fcf83114ee0ae43
Gustavo Galarreta <gustavogalarreta@Gustavos-MacBook-Pro.local>
2018-07-13 11:57:08 -0500

    fb

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -3,6 +3,9 @@ class MessengerController < ApplicationController
   protect_from_forgery with: :null_session
 
   def index
+    p params
+    p params["hub.verify_token"]
+    p params["hub.challenge"]
     if params["hub.verify_token"] == "Z4Q-L7t"
       render text: params["hub.challenge"]
     end
